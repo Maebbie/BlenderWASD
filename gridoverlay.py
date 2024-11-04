d1ecdeb703b411ddfbefa994d6c4d61b178c0f68
Maebbie <Maebbie@gmail.com>
2024-11-04 21:50:36 +0100

    modified script with more precise decimals and a lock button for the slider

--- gridoverlay.py
+++ gridoverlay.py
@@ -1,27 +1,39 @@
 import bpy
 
 class HelloWorldPanel(bpy.types.Panel):
-    """Creates a Panel in the Object properties window"""
-    bl_label = "Professional Gridsize Panel"
+    bl_label = "Grid Scale Panel"
     bl_idname = "OBJECT_PT_hello"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
 
     def draw(self, context):
         layout = self.layout
-
         overlay = bpy.context.space_data.overlay
+        scene = context.scene
 
         row = layout.row()
-        row.prop(overlay, "grid_scale")
+        row.enabled = not scene.grid_lock
+        row.prop(overlay, "grid_scale", text="")
+
+        row = layout.row()
+        row.alignment = 'LEFT'
+        row.prop(scene, "grid_lock", text="Lock Slider", toggle=True)
+        row.alignment = 'RIGHT'
+        row.label(text=f"{overlay.grid_scale:.8f}")
+        
 
 def register():
     bpy.utils.register_class(HelloWorldPanel)
-
+    bpy.types.Scene.grid_lock = bpy.props.BoolProperty(
+        name="Grid Lock",
+        description="Toggles lock on Grid Scale Slider, to avoid accidentally changing it via the slider above.",
+        default=False
+    )
 
 def unregister():
     bpy.utils.unregister_class(HelloWorldPanel)
-
+    del bpy.types.Scene.grid_lock
 
 if __name__ == "__main__":
     register()
+    
